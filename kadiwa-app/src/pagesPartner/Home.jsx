5d06929bae5a766332c985cd249c2f5e092058cc
K G A N <86733485+kganallinone@users.noreply.github.com>
2024-02-05 10:46:36 +0800

    a
    a

--- kadiwa-app/src/pagesPartner/Home.jsx
+++ kadiwa-app/src/pagesPartner/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Grid, TextField, p } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
@@ -22,8 +22,30 @@ import { getDatabase, get, ref, update } from 'firebase/database';
 import firebaseDB from '../Configuration/config-firebase2';
 
 const Home = () => {
+  const [userDetails, setUserDetails] = useState(null);
   const kdwconnect = sessionStorage.getItem('kdwconnect');
   const navigate  = useNavigate();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const snapshot = await get(ref(firebaseDB, 'kadiwa_users_account'));
+        const userData = snapshot.val();
+
+        if (userData && userData[kdwconnect]) {
+          setUserDetails(userData[kdwconnect]);
+        } else {
+          console.error('User not found');
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    if (kdwconnect) {
+      fetchData();
+    }
+  }, [kdwconnect]);
 
   const setSessionAndRedirect = (path) => {
     // Implement your session logic and redirection
@@ -73,13 +95,14 @@ const Home = () => {
       {/* Top Navigation with Search and Notification */}
       <div className="p-4 flex items-center justify-between bg-gray-100">
         {/* Search Input */}
+        {userDetails && (
         <div className="flex-grow">
           <p variant="h6" className="flex text-gray-700 font-bold">
             <Store sx={{ color: 'gray.500', mr: 1 }} />
-            SarisariStore
+            {userDetails.storeName}
           </p>
         </div>
-
+        )}
         {/* Notification Icon */}
         <div className="ml-4">
           <Logout sx={{ color: 'gray.700', cursor: 'pointer' }} onClick={logoutUser} />
@@ -129,7 +152,7 @@ const Home = () => {
       <div className="p-4 items-center justify-around bg-green-200 m-2 rounded text-green-900">
         <div className="text-left w-full bg-white shadow-md rounded-xl p-2 mb-2">
           <p variant="h6" className="font-bold">
-            PHP 0.00
+            ₱ 0.00
           </p>
           <p variant="caption">Total Cash</p>
         </div>
@@ -137,27 +160,27 @@ const Home = () => {
         {/* Cash Details */}
         <div className="grid grid-cols-4 md:grid-cols-4 gap-1">
           <div className="text-center bg-white shadow-md rounded-xl p-1">
-            <p variant="caption">Cash on Hand</p>
+            <p variant="caption" className='text-xs'>Cash</p>
             <p variant="caption" className="font-semibold">
-              Php 0.00
+            ₱ 0.00
             </p>
           </div>
           <div className="text-center bg-white shadow-md rounded-xl p-1">
-            <p variant="caption">Gcash</p>
+            <p variant="caption" className='text-xs'>Gcash</p>
             <p variant="caption" className="font-semibold">
-              Php 0.00
+            ₱ 0.00
             </p>
           </div>
           <div className="text-center bg-white shadow-md rounded-xl p-1">
-            <p variant="caption">Maya</p>
+            <p variant="caption " className='text-xs'>Maya</p>
             <p variant="caption" className="font-semibold">
-              Php 0.00
+            ₱ 0.00
             </p>
           </div>
           <div className="text-center bg-white shadow-md rounded-xl p-1">
-            <p variant="caption">Cash in Bank</p>
+            <p variant="caption" className='text-xs'>Bank</p>
             <p variant="caption" className="font-semibold">
-              Php 0.00
+            ₱ 0.00
             </p>
           </div>
         </div>
@@ -211,29 +234,28 @@ const Home = () => {
           <div className="w-full bg-green-300 h-0.5"></div>
           <span className="text-lg font-bold">₱ 0</span>
         </div>
-
-        {/* Orders Delivered Card */}
         <div className="flex flex-col items-center bg-white p-4 rounded text-green-800 shadow-md">
-          <span className="text-xs">Orders Delivered</span>
-          <div className="w-full bg-green-300 h-0.5"></div>
-          <span className="text-lg font-bold">0</span>
-        </div>
-
-        {/* Consumers Card */}
-        <div className="flex flex-col items-center bg-white p-4 rounded text-green-800 shadow-md">
-          <span className="text-xs">Consumers</span>
-          <div className="w-full bg-green-300 h-0.5"></div>
-          <span className="text-lg font-bold">0</span>
-        </div>
-
-        {/* Average Order Value Card */}
-        <div className="flex flex-col items-center bg-white p-4 rounded text-green-800 shadow-md">
-          <span className="text-xs">Average Order Value</span>
+          <span className="text-xs">Total Revenue</span>
           <div className="w-full bg-green-300 h-0.5"></div>
           <span className="text-lg font-bold">₱ 0</span>
         </div>
-      </div>
 
+             {/* Average Order Value Card */}
+        <div className="flex flex-col items-center bg-white p-4 rounded text-green-800 shadow-md">
+          <span className="text-xs">Tolal KDW PTS</span>
+          <div className="w-full bg-green-300 h-0.5"></div>
+          <span className="text-lg font-bold"> 0</span>
+        </div>
+
+        {/* Orders Delivered Card */}
+        <div className="flex flex-col items-center bg-white p-4 rounded text-green-800 shadow-md">
+          <span className="text-xs">Total Orders</span>
+          <div className="w-full bg-green-300 h-0.5"></div>
+          <span className="text-lg font-bold">0</span>
+        </div>
+
+
+        </div>
       
     </div>
   );
