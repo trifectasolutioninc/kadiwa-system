261c6619c868dd58158b2caf4f75aa6cca8460b7
K G A N <86733485+kganallinone@users.noreply.github.com>
2024-02-16 11:27:33 +0800

    a
    a

--- kadiwa-app/src/Scripts/connections/check.js
+++ kadiwa-app/src/Scripts/connections/check.js
@@ -1,16 +1,15 @@
 // sessionUtils.js
 const redirectToIndexIfNoConnect = () => {
-    const kdwconnect = sessionStorage.getItem('kdwconnect');
     const uid = sessionStorage.getItem('uid');
     const sid = sessionStorage.getItem('sid');
     
-    if (kdwconnect === null || kdwconnect === '') {
+    if (uid === null || uid === '') {
       // Redirect to the index page
       window.location.href = '/'; // Adjust the path accordingly
       return false; // Indicate that the redirection is performed
     } else {
       // Use the username as needed in your dashboard page
-      console.log('userID: ' + kdwconnect);
+      console.log('userID: ' + uid);
       console.log('uid: ' + uid);
       console.log('sid: ' + sid);
       return true; // Indicate that the contact is valid
