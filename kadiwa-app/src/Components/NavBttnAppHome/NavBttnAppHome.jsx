ce2c73e9cf986aa6d0ebb2edce17528be7b98997
Argel <trifectasolutioninc.db@gmail.com>
2024-02-16 14:54:47 +0800

    aaa

--- kadiwa-app/src/Components/NavBttnAppHome/NavBttnAppHome.jsx
+++ kadiwa-app/src/Components/NavBttnAppHome/NavBttnAppHome.jsx
@@ -1,18 +1,55 @@
-import React from 'react';
-import { Home, Store, Chat, ShoppingCart, AccountCircle } from '@mui/icons-material';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import {
+  Home,
+  Store,
+  Chat,
+  ShoppingCart,
+  AccountCircle,
+} from "@mui/icons-material";
+import { NavLink } from "react-router-dom";
 
 const NavBttnAppHome = () => {
   return (
     <React.Fragment>
-      <footer className="p-4 text-white flex justify-around fixed bottom-0 w-full" style={{ backgroundColor: '#20802F' }}>
-        <NavLink to="" className="text-white">
+      <footer
+        className="p-4 text-white flex items-center  justify-around fixed bottom-0 w-full"
+        style={{ backgroundColor: "#20802F" }}
+      >
+        <NavLink
+          to=""
+          className="text-white text-xs flex flex-col items-center"
+        >
           <Home />
+          Home
         </NavLink>
-        <NavLink to="store" className="text-white"><Store /></NavLink>
-        <NavLink to="chat" className="text-white"><Chat /></NavLink>
-        <NavLink to="cart" className="text-white"><ShoppingCart /></NavLink>
-        <NavLink to="profile" className="text-white"><AccountCircle /></NavLink>
+        <NavLink
+          to="store"
+          className="text-white text-xs flex flex-col items-center"
+        >
+          <Store />
+          Stores
+        </NavLink>
+        <NavLink
+          to="chat"
+          className="text-white text-xs flex flex-col items-centere"
+        >
+          <Chat />
+          Chat
+        </NavLink>
+        <NavLink
+          to="cart"
+          className="text-white text-xs flex flex-col items-center"
+        >
+          <ShoppingCart />
+          Cart
+        </NavLink>
+        <NavLink
+          to="profile"
+          className="text-white text-xs flex flex-col items-center"
+        >
+          <AccountCircle />
+          Account
+        </NavLink>
       </footer>
     </React.Fragment>
   );
--- kadiwa-app/src/pagesConsumer/Home.jsx
+++ kadiwa-app/src/pagesConsumer/Home.jsx
@@ -1,13 +1,12 @@
 // Import necessary modules
-import React, { useState, useEffect } from 'react';
-import { LocationOn, Search, Notifications } from '@mui/icons-material';
-import { imageConfig , commodityTypes } from '../Configuration/config-file';
-import configFirebaseDB from '../Configuration/config';
-import { ref, child, get } from 'firebase/database';
-import { Link } from 'react-router-dom'; 
+import React, { useState, useEffect } from "react";
+import { LocationOn, Search, Notifications } from "@mui/icons-material";
+import { imageConfig, commodityTypes } from "../Configuration/config-file";
+import configFirebaseDB from "../Configuration/config";
+import { ref, child, get } from "firebase/database";
+import { Link } from "react-router-dom";
 
 const HomeConsumer = () => {
-
   const [selectedCommodity, setSelectedCommodity] = useState("All Commodities");
   const [products, setProducts] = useState([]);
   const [userLocation, setUserLocation] = useState("Loading...");
@@ -26,82 +25,99 @@ const HomeConsumer = () => {
   };
 
   const displayProducts = (commodityType) => {
-    const productsRef = ref(database, 'products_info');
-        // Use the 'value' event to fetch data once
-    get(productsRef).then((snapshot) => {
+    const productsRef = ref(database, "products_info");
+    // Use the 'value' event to fetch data once
+    get(productsRef)
+      .then((snapshot) => {
         const productsData = snapshot.val() || {};
-    
+
         // Assuming you have a specific node structure in your database
-        const filteredProducts = Object.values(productsData)
-            .filter(product => commodityType === 'All Commodities' || product.commodity_type === commodityType);
-    
+        const filteredProducts = Object.values(productsData).filter(
+          (product) =>
+            commodityType === "All Commodities" ||
+            product.commodity_type === commodityType
+        );
+
         setProducts(filteredProducts);
-        }).catch(error => {
-        console.error('Error fetching and filtering products:', error);
-        });
+      })
+      .catch((error) => {
+        console.error("Error fetching and filtering products:", error);
+      });
   };
 
-   const fetchUserLocation = () => {
+  const fetchUserLocation = () => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
-        position => {
+        (position) => {
           const userLatitude = position.coords.latitude;
           const userLongitude = position.coords.longitude;
 
-          fetch(`https://nominatim.openstreetmap.org/reverse?lat=${userLatitude}&lon=${userLongitude}&format=json`)
-            .then(response => response.json())
-            .then(data => {
+          fetch(
+            `https://nominatim.openstreetmap.org/reverse?lat=${userLatitude}&lon=${userLongitude}&format=json`
+          )
+            .then((response) => response.json())
+            .then((data) => {
               const address = data.display_name;
               const formattedAddress = `${address}`;
               setUserLocation(formattedAddress);
             })
-            .catch(error => {
-              console.error('Error fetching location:', error);
-              setUserLocation('Unable to retrieve location');
+            .catch((error) => {
+              console.error("Error fetching location:", error);
+              setUserLocation("Unable to retrieve location");
             });
         },
-        error => {
-          console.error('Error getting user location:', error);
-          setUserLocation('Unable to retrieve location');
+        (error) => {
+          console.error("Error getting user location:", error);
+          setUserLocation("Unable to retrieve location");
         },
         { enableHighAccuracy: true, timeout: 5000, maximumAge: 0 }
       );
     } else {
-      console.error('Geolocation is not supported by this browser.');
-      setUserLocation('Geolocation not supported');
+      console.error("Geolocation is not supported by this browser.");
+      setUserLocation("Geolocation not supported");
     }
   };
 
-
   return (
-    <div className="h-screen bg-gray-100">
-         <div id="topView" className="p-4">
+    <div className="p-5 bg-gray-100 space-y-5">
+      <div id="topView" className="space-y-3">
         <h1 className="text-green-700 font-bold">Hello Kadiwa User!</h1>
-        <div id="userLocation" className="flex items-center mb-1 text-xs">
+        <div id="userLocation" className="flex items-center text-xs">
           <LocationOn className="text-gray-700 mr-2" />
-          <span id="userLocationText" className="text-gray-600">{userLocation}</span>
+          <span id="userLocationText" className="text-gray-600">
+            {userLocation}
+          </span>
         </div>
       </div>
-      <div className='px-4 pb-2'>
-        <img src={imageConfig.BannerV1} alt=""  className='rounded-md'/>
+      <div className="">
+        <img src={imageConfig.BannerV1} alt="" className="rounded-md" />
       </div>
 
-      <div className="px-4 flex items-center justify-around ">
+      <div className=" flex items-center justify-around ">
         <div className="flex-grow flex items-center">
           <div className="relative flex items-center bg-gray-300 rounded-md p-2 flex-grow">
             <Search className="text-gray-700 text-lg mr-2" />
-            <input type="text" placeholder="Search..." className="w-full bg-gray-300 text-gray-600 focus:outline-none" />
+            <input
+              type="text"
+              placeholder="Search..."
+              className="w-full bg-gray-300 text-gray-600 focus:outline-none"
+            />
           </div>
         </div>
         <div className="ml-4">
           <Notifications className="text-gray-700 text-lg" />
         </div>
       </div>
-      <div className="overflow-x-auto w-screen flex px-2">
+
+      <div className="overflow-x-auto flex gap-5">
         {commodityTypes.map((commodityType, index) => (
           <button
             key={index}
-            className={`border-green-700 border ${selectedCommodity === commodityType ? 'bg-green-700 text-white' : 'text-green-700'} text-xs rounded py-2 px-4 m-2 w-auto whitespace-nowrap tab-button`}
+            className={`border-green-700 border ${
+              selectedCommodity === commodityType
+                ? "bg-green-700 text-white"
+                : "text-green-700"
+            } text-xs rounded py-2 px-4 w-auto whitespace-nowrap tab-button`}
             data-commodity-type={commodityType}
             onClick={() => handleCommodityClick(commodityType)}
           >
@@ -110,10 +126,19 @@ const HomeConsumer = () => {
         ))}
       </div>
 
-      <div id="productlist" className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4 mb-16">
+      <div
+        id="productlist"
+        className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"
+      >
         {products.map((product, index) => (
-          <div key={index} className="container mx-auto bg-white rounded-lg shadow-lg overflow-hidden">
-            <div className="h-full">
+          <div
+            key={index}
+            className="container p-5  bg-white rounded-lg shadow-md"
+          >
+            <Link
+              to={`/main/productinfo/${product.product_code}`}
+              className="space-y-5"
+            >
               <div className="text-left h-1/2">
                 <img
                   id={`product${product.product_code}`}
@@ -122,22 +147,29 @@ const HomeConsumer = () => {
                   src={imageConfig[product.keywords.toLowerCase()]}
                 />
               </div>
-              <div className="text-left h-1/2 flex flex-col justify-end p-2">
-                <h2 className="text-xs font-semibold">{product.product_name}</h2>
-                <p className="text-xs font-semibold text-gray-500">{product.commodity_type}</p>
-                <p className="text-xs font-bold text-green-600">Php {product.price.toFixed(2)}</p>
-                <Link to={`/main/productinfo/${product.product_code}`}
-                  className="text-xs font-semibold text-green-500 cursor-pointer border text-center border-green-600 p-2 rounded-md mt-1">
+              <div className="space-y-2">
+                <h2 className="font-semibold truncate">
+                  {product.product_name}
+                </h2>
+                <p className=" font-semibold text-gray-500 truncate">
+                  {product.commodity_type}
+                </p>
+                <p className=" font-bold text-green-600">
+                  Php {product.price.toFixed(2)}
+                </p>
+                {/* <Link className="text-xs font-semibold text-green-500 cursor-pointer border text-center border-green-600 p-2 rounded-md mt-1">
                   View
-                  </Link>
+                </Link> */}
               </div>
-            </div>
+            </Link>
           </div>
         ))}
       </div>
-      
+      <br />
+      <br />
+      <br />
     </div>
-  )
-}
+  );
+};
 
-export default HomeConsumer
+export default HomeConsumer;
