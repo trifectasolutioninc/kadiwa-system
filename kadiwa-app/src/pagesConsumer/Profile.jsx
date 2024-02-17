22fd816d42e738237a9c09cd3de36e1af4e69243
Argel <trifectasolutioninc.db@gmail.com>
2024-02-17 15:56:07 +0800

    update

--- kadiwa-app/src/pagesConsumer/Profile.jsx
+++ kadiwa-app/src/pagesConsumer/Profile.jsx
@@ -1,41 +1,57 @@
-import React, { useEffect, useState } from 'react';
-import { Add, Info, CreditCard, Link as LinkIcon, History, LocalShipping, LocalMall, Done, Settings, Store } from '@mui/icons-material';
-import { Avatar, Badge } from '@mui/material';
-import { Button } from '@mui/material';
-import { Link , NavLink } from 'react-router-dom';
-import firebaseDB from '../Configuration/config';
-import { ref, child, get } from 'firebase/database';
-import redirectToIndexIfNoConnect from '../Scripts/connections/check';
+import React, { useEffect, useState } from "react";
+import {
+  Add,
+  Info,
+  CreditCard,
+  Link as LinkIcon,
+  History,
+  LocalShipping,
+  LocalMall,
+  Done,
+  Settings,
+  Store,
+  Timer,
+} from "@mui/icons-material";
+import { Avatar, Badge } from "@mui/material";
+import { Button } from "@mui/material";
+import { Link, NavLink } from "react-router-dom";
+import firebaseDB from "../Configuration/config";
+import { ref, child, get } from "firebase/database";
+import redirectToIndexIfNoConnect from "../Scripts/connections/check";
 import { FaBox } from "react-icons/fa";
 import { IoMdArrowRoundBack } from "react-icons/io";
 import { IoWalletOutline } from "react-icons/io5";
 import { MdInsertLink } from "react-icons/md";
 import { CiCreditCard1 } from "react-icons/ci";
+import BackButton from "./BackToHome";
 
 const ProfileConsumer = () => {
   const [userData, setUserData] = useState(null);
   const [userwalletData, setUserWalletData] = useState(null);
   const [userstoreData, setUserstoreData] = useState(null);
 
-  useEffect(() => {   
+  useEffect(() => {
     const fetchUserData = async () => {
       if (!redirectToIndexIfNoConnect()) {
         return;
       }
 
       const database = firebaseDB();
-      const usersAccountRef = ref(database, 'users_information');
-      const usersWalletRef = ref(database, 'user_wallet');
-      const usersStoreRef = ref(database, 'store_information');
+      const usersAccountRef = ref(database, "users_information");
+      const usersWalletRef = ref(database, "user_wallet");
+      const usersStoreRef = ref(database, "store_information");
 
       try {
-        const uid = sessionStorage.getItem('uid');
-        const sid = sessionStorage.getItem('sid');
+        const uid = sessionStorage.getItem("uid");
+        const sid = sessionStorage.getItem("sid");
         const userSnapshot = await get(child(usersAccountRef, uid));
         const userwalletSnapshot = await get(child(usersWalletRef, uid));
         const userstoreSnapshot = await get(child(usersStoreRef, sid));
 
-        if ((userSnapshot.exists() && userwalletSnapshot.exists()) || userstoreSnapshot.exists() ) {
+        if (
+          (userSnapshot.exists() && userwalletSnapshot.exists()) ||
+          userstoreSnapshot.exists()
+        ) {
           const userData = userSnapshot.val();
           const userWalletData = userwalletSnapshot.val();
           const userstoreData = userstoreSnapshot.val();
@@ -44,59 +60,64 @@ const ProfileConsumer = () => {
           setUserstoreData(userstoreData);
           updateHTMLWithUserData(userData, userWalletData, userstoreData); // Call the function to update HTML
         } else {
-          console.error('User not found');
+          console.error("User not found");
         }
       } catch (error) {
-        console.error('Error fetching user data:', error);
+        console.error("Error fetching user data:", error);
       }
     };
 
-    const updateHTMLWithUserData = (userData, userWalletData, userstoreData) => {
-      if ((!userData && !userstoreData) && !userWalletData) {
+    const updateHTMLWithUserData = (
+      userData,
+      userWalletData,
+      userstoreData
+    ) => {
+      if (!userData && !userstoreData && !userWalletData) {
         return;
       }
-    
-      const typeofuserElement = document.getElementById('typeofuser');
-      const fullnameElement = document.getElementById('fullname');
-      const contactElement = document.getElementById('contact');
-      const balanceElement = document.getElementById('balance');
-      const ptsElement = document.getElementById('points');
-      const applyPartnerElement = document.getElementById('applyPartner');
-      const storePartnerElement = document.getElementById('storePartner');
-      const storeNameElement = document.getElementById('storeName');
-    
+
+      const typeofuserElement = document.getElementById("typeofuser");
+      const fullnameElement = document.getElementById("fullname");
+      const contactElement = document.getElementById("contact");
+      const balanceElement = document.getElementById("balance");
+      const ptsElement = document.getElementById("points");
+      const applyPartnerElement = document.getElementById("applyPartner");
+      const storePartnerElement = document.getElementById("storePartner");
+      const storeNameElement = document.getElementById("storeName");
+
       if (typeofuserElement) {
-        typeofuserElement.textContent = userData.type || 'No UserType';
+        typeofuserElement.textContent = userData.type || "No UserType";
       }
 
       if (storeNameElement) {
-        storeNameElement.textContent = userstoreData.name || 'No Store';
+        storeNameElement.textContent = userstoreData.name || "No Store";
       }
-    
+
       if (fullnameElement) {
-        fullnameElement.textContent = userData.first_name + " " + userData.last_name || 'No Name';
+        fullnameElement.textContent =
+          userData.first_name + " " + userData.last_name || "No Name";
       }
-    
+
       if (contactElement) {
-        contactElement.textContent = userData.contact || 'No Contact';
+        contactElement.textContent = userData.contact || "No Contact";
       }
-    
+
       if (balanceElement) {
         balanceElement.textContent = `PHP ${userwalletData.balance}`;
       }
-    
+
       if (ptsElement) {
         ptsElement.textContent = `KDW ${userwalletData.points}`;
       }
-    
+
       if (applyPartnerElement && storePartnerElement) {
         console.log(userData.type);
-        if (userData.type === 'consumer') {
-          applyPartnerElement.classList.remove('hidden');
-          storePartnerElement.classList.add('hidden');
-        } else if (userData.type === 'partner') {
-          applyPartnerElement.classList.add('hidden');
-          storePartnerElement.classList.remove('hidden');
+        if (userData.type === "consumer") {
+          applyPartnerElement.classList.remove("hidden");
+          storePartnerElement.classList.add("hidden");
+        } else if (userData.type === "partner") {
+          applyPartnerElement.classList.add("hidden");
+          storePartnerElement.classList.remove("hidden");
         }
       }
     };
@@ -104,62 +125,60 @@ const ProfileConsumer = () => {
     const fetchDataInterval = setInterval(() => {
       fetchUserData();
     }, 1000); // Fetch data every 5 seconds (adjust interval as needed)
-  
+
     // Cleanup function to clear interval when component unmounts
     return () => {
       clearInterval(fetchDataInterval);
     };
-    
-
-   
   }, []);
 
   return (
-    <div className="h-auto bg-gray-100 px-4">
-      <div className='flex pt-4 mb-1 items-center '>
-        <NavLink to={"/main"} className=''>
-          <IoMdArrowRoundBack />
-        </NavLink>
-        <h1 className="text-lg text-green-600  font-bold">Profile</h1>
-
+    <main className="p-3 md:px-10 space-y-5">
+      <div className="flex items-center gap-5">
+        <BackButton />
+        <h1 className="text-xl text-green-600  font-bold">Profile</h1>
       </div>
 
       {/* Profile Information */}
       {userData && (
-        <div className="relative p-4 flex justify-between items-center bg-white m-4 rounded-md shadow-md ">
-          <div>
+        <section className="relative p-4 flex items-center justify-between bg-white rounded-md shadow-md ">
+          <div className="flex flex-col items-center gap-2">
             {/* Display Picture */}
             <Avatar className="w-12 h-12 rounded-full" />
-          </div>
-          <div className="ml-4 mt-2">
-            {/* Display Name */}
-            <h1 id="fullname" className="font-bold text-lg">
-              {userData.first_name + " " + userData.last_name}
-            </h1>
-            {/* Display Contact */}
-            <p id="contact" className="text-gray-600">
-              {userData.contact}
-            </p>
-          </div>
-          <div className="flex items-center">
-            <div className="absolute top-0 right-0 p-2">
+            <div className="">
               {/* Display User Type */}
               <p
                 id="typeofuser"
-                className="rounded-3xl p-1 text-xs text-gray-800"
-                style={{ backgroundColor: '#54FC6F' }}
+                className="w-fit text-center rounded-full px-2 py-1 text-xs text-black/80"
+                style={{ backgroundColor: "#54FC6F" }}
               >
                 {userData.type}
               </p>
             </div>
-            <div className="ml-2 mt-4">
+          </div>
+
+          <div>
+            <div className="">
+              {/* Display Name */}
+              <h1 id="fullname" className="font-bold text-lg">
+                {userData.first_name + " " + userData.last_name}
+              </h1>
+              {/* Display Contact */}
+              <p id="contact" className="text-black/80 font-semibold">
+                {userData.contact}
+              </p>
+            </div>
+          </div>
+
+          <div className="flex items-center">
+            <div className="">
               {/* Make the settings icon clickable */}
               <Link to="/route/profileinfo">
                 <Settings className="text-gray-500" />
               </Link>
             </div>
           </div>
-        </div>
+        </section>
       )}
 
       {/* <div id="applyPartner" className="bg-green-300 mx-4 p-2 justify-between flex items-center rounded ">
@@ -177,13 +196,13 @@ const ProfileConsumer = () => {
         <Link to="/partner/home" id="storeButton" className="bg-blue-500 py-1  text-white rounded ">Store</Link>
       </div> */}
 
-      <div className=" flex justify-between mt-4">
+      <div className=" flex justify-between">
         <h1 className="font-bold text-lg text-gray-800">My Wallet</h1>
         <div></div>
       </div>
 
       {userwalletData && (
-        <div className="p-4 flex justify-between bg-white m-4 rounded-md shadow-md">
+        <div className="p-4 flex justify-between bg-white rounded-md shadow-md">
           {/* Display PHP Amount */}
           <p id="balance" className="text-gray-600 font-semibold mr-2">
             PHP {userwalletData.balance}
@@ -194,7 +213,7 @@ const ProfileConsumer = () => {
       )}
 
       {userwalletData && (
-        <div className="p-4 flex justify-between bg-white m-4 rounded-md shadow-md">
+        <div className="p-4 flex justify-between bg-white rounded-md shadow-md">
           {/* Display PHP Amount */}
           <p id="points" className="text-gray-600 font-semibold mr-2">
             KDW {userwalletData.points}
@@ -204,62 +223,73 @@ const ProfileConsumer = () => {
         </div>
       )}
 
-      <div className="flex justify-around  py-8">
+      <section className="grid grid-cols-3 gap-5 my-10">
         {/* Icon with Name: Virtual Card */}
-        <div className="text-center">
-        <Link to="/main/virtual-card" className="block text-center">
+
+        <Link
+          to="/main/virtual-card"
+          className="text-center border rounded-md p-3 w-full bg-white shadow-md hover:bg-green-300"
+        >
           <CiCreditCard1 className="text-3xl mx-auto text-gray-500 mb-2" />
-          <p className="text-xs">Virtual Card</p>
+          <p className="text-black/80 text-sm">Virtual Card</p>
         </Link>
-      </div>
 
         {/* Icon with Name: Linked Account */}
-        <div className="text-center">
-          <Link to="/main/linked-account" className="block text-center">
-            <MdInsertLink className="text-3xl mx-auto text-gray-500 mb-2" />
-            <p className="text-xs">Linked Account</p>
-          </Link>
-        </div>
 
+        <Link
+          to="/main/linked-account"
+          className="text-center border rounded-md p-3 w-full bg-white shadow-md hover:bg-green-300"
+        >
+          <MdInsertLink className="text-3xl mx-auto text-gray-500 mb-2" />
+          <p className="text-black/80 text-sm whitespace-nowrap">
+            Linked Account
+          </p>
+        </Link>
 
         {/* Icon with Name: Transaction History */}
-        <div className="text-center">
-          <IoWalletOutline className="text-3xl mx-auto text-gray-500 mb-2" />
-          <p className="text-xs">Load</p>
-        </div>
-      </div>
 
-      <div className=" flex justify-between py-2">
+        <Link className="text-center border rounded-md p-3 w-full bg-white shadow-md hover:bg-green-300">
+          <IoWalletOutline className="text-3xl mx-auto text-gray-500 mb-2" />
+          <p className="text-black/80 text-sm ">Load</p>
+        </Link>
+      </section>
+
+      <div className=" flex justify-between">
         <h1 className="font-bold text-lg text-gray-800">My Orders</h1>
         <div></div>
       </div>
-      <div className="mx-4 grid grid-cols-2 md:grid-cols-3 gap-4 ">
+      <section className="grid grid-cols-2 md:grid-cols-3 gap-4">
         <Link
           to={"/main/orders/delivery"}
-          className="px-4 py-2 bg-white rounded-md border-green-500 text-[0.8em] border flex flex-col items-center justify-center gap-3"
+          className="px-4 py-2 bg-white rounded-md border-green-500 text-[0.8em] border shadow-md flex flex-col items-center justify-center gap-3"
         >
           <LocalShipping className="text-[0.8em] text-gray-500" />
           Delivery
         </Link>
         <Link
           to={"/main/orders/pickup"}
-          className="px-4 py-2 bg-white rounded-md border-green-500 text-[0.8em] border flex flex-col items-center justify-center gap-3"
+          className="px-4 py-2 bg-white rounded-md border-green-500 text-[0.8em] border shadow-md flex flex-col items-center justify-center gap-3"
         >
           <FaBox className=" text-gray-500" />
           Pickup
         </Link>
         <Link
           to={"/main/orders/history"}
-          className="px-4 py-2 bg-white rounded-md border-green-500 text-[0.8em] border flex flex-col items-center justify-center gap-3 whitespace-nowrap"
+          className="px-4 py-2 bg-white rounded-md border-green-500 text-[0.8em] border shadow-md flex flex-col items-center justify-center gap-3 whitespace-nowrap"
         >
           <History className=" text-gray-500" />
           Transaction History
         </Link>
-      </div>
-      <div className='p-2 h-32'> 
-
-      </div>
-    </div>
+        <Link
+          to={"/main/orders/history"}
+          className="px-4 py-2 bg-white rounded-md border-green-500 text-[0.8em] border shadow-md flex flex-col items-center justify-center gap-3 whitespace-nowrap"
+        >
+          <Timer className=" text-gray-500" />
+          Scheduled Delivery
+        </Link>
+      </section>
+      <div className="p-2 h-32"></div>
+    </main>
   );
 };
 
--- kadiwa-app/src/pagesConsumer/StoreMap.jsx
+++ kadiwa-app/src/pagesConsumer/StoreMap.jsx
@@ -17,7 +17,7 @@ function StoreMap() {
         />
       </LoadScript> */}
 
-      <section className="p-4 space-y-2 rounded-md bg-white shadow-md">
+      <section className="p-2 space-y-2 rounded-md bg-white shadow-md">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7722.346360333354!2d120.96923591578918!3d14.589205726278434!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397ca3d1375e1fb%3A0x49ebfa658c0ba08!2sIntramuros%2C%20Manila%2C%201002%20Metro%20Manila!5e0!3m2!1sen!2sph!4v1708141259401!5m2!1sen!2sph"
           className="w-full h-50 md:h-80 lg:h-96 xl:h-80 2xl:h-96" // Responsive height
