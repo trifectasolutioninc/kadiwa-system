e5918e076b7933519a585481d1dbd082a794d3b8
K G A N <86733485+kganallinone@users.noreply.github.com>
2024-02-20 16:12:51 +0800

    a
    a

--- kadiwa-app/src/pagesConsumer/Profile/Account_Management/PersonalInfoPage.jsx
+++ kadiwa-app/src/pagesConsumer/Profile/Account_Management/PersonalInfoPage.jsx
@@ -3,24 +3,24 @@ import React, { useState, useEffect } from 'react';
 import { IoMdArrowRoundBack } from 'react-icons/io';
 import { NavLink } from 'react-router-dom';
 const PersonalInfoPage = () => {
-  return (
-    <div className=' bg-gray-100 h-screen'> 
-        <div>
-        <div className="bg-white pb-2 shadow-md top-0 fixed w-full">
-      <div className="flex items-center gap-5 p-3">
-            <NavLink to={"/route/profileedit"} className="">
-              <IoMdArrowRoundBack fontSize={"25px"} />
-            </NavLink>
-            <h1 className="text-lg text-green-600 font-bold">
-              Edit Profile
-            </h1>
-          </div></div>
+    return (
+        <div className=' bg-gray-100 h-screen'>
+            <div>
+                <div className="bg-white pb-2 shadow-md top-0 fixed w-full">
+                    <div className="flex items-center gap-5 p-3">
+                        <NavLink to={"/route/profileedit"} className="">
+                            <IoMdArrowRoundBack fontSize={"25px"} />
+                        </NavLink>
+                        <h1 className="text-lg text-green-600 font-bold">
+                            Edit Profile
+                        </h1>
+                    </div></div>
+
+            </div>
+
 
         </div>
-        
-        
-    </div>
-  )
+    )
 }
 
 export default PersonalInfoPage;
