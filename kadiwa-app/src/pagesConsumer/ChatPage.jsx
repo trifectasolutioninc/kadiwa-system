bcf87b451e84112f8c23c921c3805d65766a4b79
K G A N <86733485+kganallinone@users.noreply.github.com>
2024-02-22 00:58:04 +0800

    a
    a

--- kadiwa-app/src/pagesConsumer/ChatPage.jsx
+++ kadiwa-app/src/pagesConsumer/ChatPage.jsx
@@ -8,7 +8,7 @@ import { ChatBOT } from "../services/AI/chat-bot";
 
 const ChatPage = () => {
   const maxTextareaHeight = 120;
-  const { storeID , page } = useParams();
+  const { storeID, page } = useParams();
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState("");
   const [storeName, setStoreName] = useState("");
@@ -64,51 +64,49 @@ const ChatPage = () => {
   useEffect(() => {
     const chatId = `${uid}_${storeID}`;
     const chatRef = ref(firebaseDB, `chat_collections/${chatId}`);
-  
+
     const handleNewMessage = async (snapshot) => {
       const chatData = snapshot.val();
-  
+
       if (chatData && chatData.Chat) {
         const messagesArray = Object.values(chatData.Chat).map((message) => {
           return {
             ...message,
-            id: message.time // Add unique ID for each message
+            id: message.time
           };
         });
-  
-        // Iterate through messages to update status to "read" for bot and partner messages
+
+
         const updatedMessages = messagesArray.map((message) => {
           if ((message.sender === "bot" || message.sender === "partner") && message.status !== "read") {
             return { ...message, status: "read" };
           }
           return message;
         });
-  
+
         setMessages(updatedMessages);
-  
-        // Update the status of bot and partner messages in the database
+
         const updates = {};
         updatedMessages.forEach(message => {
           updates[`Chat/${message.id}/status`] = message.status;
         });
-        await update(chatRef, updates); // Corrected update call here
+        await update(chatRef, updates);
       } else if (messages.length === 0) {
-        // If there are no messages, initiate conversation with bot
+
         sendChatMessage("Good Day! Welcome to our store!", "bot");
       }
     };
-  
+
     onValue(chatRef, handleNewMessage);
-  
+
     return () => {
-      // Detach the event listener when component unmounts
       off(chatRef, "value", handleNewMessage);
     };
   }, [storeID, storeName, uid, messages.length]);
-  
-  
-  
-  
+
+
+
+
   useEffect(() => {
     if (messageEndRef.current) {
       messageEndRef.current.scrollIntoView({ behavior: "smooth" });
@@ -131,26 +129,26 @@ const ChatPage = () => {
     try {
       const chatId = `${uid}_${storeID}`;
       const chatRef = ref(firebaseDB, `chat_collections/${chatId}`);
-  
-      const timestamp = generateUniqueId(); // Use timestamp-based unique ID
-  
+
+      const timestamp = generateUniqueId();
+
       console.log("Store ID:", storeID);
       console.log("Store Name:", storeName);
-  
-      let consumerName = "Unknown"; // Default consumer name
-  
-      // Check if userDetails is not null and contains first_name
+
+      let consumerName = "Unknown";
+
+
       if (userDetails && userDetails.first_name) {
         consumerName = userDetails.first_name + " " + userDetails.last_name;
       }
-  
+
       let botReply = null;
       let maxMatchingKeywords = 0;
-  
-      // Iterate through each chat bot object
+
+
       for (const bot of Object.values(ChatBOT)) {
         let matchingKeywords = 0;
-        // Check if the message matches any keyword from the chat bot
+
         for (const keyword of bot.keywords) {
           if (message.toLowerCase().includes(keyword)) {
             matchingKeywords++;
@@ -161,21 +159,21 @@ const ChatPage = () => {
           botReply = bot;
         }
       }
-  
+
       const newMessage = {
         img: "",
         message,
-        sender: sender || "consumer", // Default sender is consumer
+        sender: sender || "consumer",
         time: timestamp,
         status: "unread",
       };
-  
-      // Get the current chat data
+
+
       const currentChatData = await get(chatRef);
       const existingChat = currentChatData.val();
       const STRN = sessionStorage.getItem('STRN');
       const STRID = sessionStorage.getItem('STRID');
-      if (storeName == "" || ownerID == "" ){
+      if (storeName == "" || ownerID == "") {
         storeName = STRN;
         ownerID = STRID;
       }
@@ -185,23 +183,23 @@ const ChatPage = () => {
         consumer: uid,
         consumerName: consumerName,
         Chat: {
-          ...existingChat?.Chat, // Keep existing messages
-          [timestamp]: newMessage, // Add the new message
+          ...existingChat?.Chat,
+          [timestamp]: newMessage,
         },
       };
-  
+
       if (botReply) {
-        // Add a delay of 3 seconds before sending the bot reply
+
         setTimeout(() => {
           const timestamp2 = generateUniqueId()
           const botMessage = {
             img: "",
             message: botReply.message,
-            sender: "bot", // Set sender as bot
-            time: timestamp2, // Use the same timestamp as the user's message
+            sender: "bot",
+            time: timestamp2,
             status: "unread",
           };
-          // Update the chatData with botMessage
+
           const updatedChatData = {
             storeName: storeName,
             storeOwner: ownerID,
@@ -212,20 +210,19 @@ const ChatPage = () => {
               [timestamp2]: botMessage,
             },
           };
-          // Set the updated data under the unique chatId
+
           update(chatRef, updatedChatData);
         }, 1000);
       }
-  
-      // Set the updated data under the unique chatId immediately for user message
+
       set(chatRef, chatData);
-  
+
       console.log("Message sent successfully!");
     } catch (error) {
       console.error("Error sending chat message:", error);
     }
   };
-  
+
   const handleSendMessage = () => {
     if (newMessage.trim() !== "") {
       setMessages([...messages, { sender: "user", text: newMessage }]);
@@ -236,7 +233,7 @@ const ChatPage = () => {
 
   const formatDate = (timestamp) => {
     if (!timestamp) {
-      return ""; // Handle the case where timestamp is undefined or null
+      return "";
     }
 
     const [year, month, day, hour, minute, second] = timestamp.split("-");
@@ -261,28 +258,28 @@ const ChatPage = () => {
   return (
     <div className="h-screen bg-gray-200 flex flex-col">
       <div className="flex gap-5 items-center p-4 bg-white shadow-md">
-      { getpage === "store" && (
+        {getpage === "store" && (
 
-        <NavLink to={`/main/storepage/${storeID}`} className="">
-          <IoMdArrowRoundBack fontSize={"25px"} />
-        </NavLink>
-
-      )}
-      { getpage === "chat" && (
-
-        <NavLink to={`/main/chat`} className="">
-          <IoMdArrowRoundBack fontSize={"25px"} />
-        </NavLink>
+          <NavLink to={`/main/storepage/${storeID}`} className="">
+            <IoMdArrowRoundBack fontSize={"25px"} />
+          </NavLink>
 
         )}
-        { getpage === "store-home" && (
+        {getpage === "chat" && (
 
-<NavLink to={`/main/store`} className="">
-  <IoMdArrowRoundBack fontSize={"25px"} />
-</NavLink>
+          <NavLink to={`/main/chat`} className="">
+            <IoMdArrowRoundBack fontSize={"25px"} />
+          </NavLink>
 
-)}
-        
+        )}
+        {getpage === "store-home" && (
+
+          <NavLink to={`/main/store`} className="">
+            <IoMdArrowRoundBack fontSize={"25px"} />
+          </NavLink>
+
+        )}
+
         <h1 className="text-gray-700 font-bold text-lg">{storeName}</h1>
       </div>
 
@@ -293,43 +290,38 @@ const ChatPage = () => {
         {messages.map((message, index) => (
           <div
             key={index}
-            className={`my-2 ${
-              message.sender === "consumer" ? "text-right" : "text-left"
-            }`}
+            className={`my-2 ${message.sender === "consumer" ? "text-right" : "text-left"
+              }`}
           >
             <div
-              className={`inline-block p-2 rounded-lg ${
-                message.sender === "consumer"
+              className={`inline-block p-2 rounded-lg ${message.sender === "consumer"
                   ? "bg-green-500 text-white"
                   : "bg-white"
-              }`}
+                }`}
             >
               <p>{message.message}</p>
             </div>
             <p
-              className={`text-xs text-gray-500 ${
-                message.sender === "consumer" ? "text-right " : "text-left"
-              }`}
+              className={`text-xs text-gray-500 ${message.sender === "consumer" ? "text-right " : "text-left"
+                }`}
             >
               {formatDate(message.time)}
             </p>
           </div>
         ))}
-        {/* Empty div used as a reference point to scroll to */}
+
         <div ref={messageEndRef}></div>
       </div>
       <div className="h-20" ></div>
-      
+
       <div className="p-4 bg-white shadow-md fixed bottom-0  w-full z-50">
         <div className="flex items-center">
           <textarea
             value={newMessage}
             onChange={(e) => {
               setNewMessage(e.target.value);
-
-              // Adjust textarea height dynamically based on content
               const textarea = e.target;
-              textarea.style.height = "50px"; // Reset height to auto
+              textarea.style.height = "50px";
               textarea.style.height = ` ${Math.min(
                 textarea.scrollHeight,
                 maxTextareaHeight
@@ -338,7 +330,7 @@ const ChatPage = () => {
             placeholder="Type your message..."
             className="flex-1 p-2 mr-2 border border-gray-300 rounded"
             style={{ height: "50px", maxHeight: `${maxTextareaHeight}px` }}
-            rows="3" // Set the number of visible rows
+            rows="3"
           />
           <button
             onClick={handleSendMessage}
