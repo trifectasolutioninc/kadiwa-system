353d56a6dd0f34a6fa440940550458312d37a0d9
Argel <trifectasolutioninc.db@gmail.com>
2024-02-21 21:42:31 +0800

    back to classic

--- kadiwa-app/src/pagesConsumer/Products/GoodsCluster.jsx
+++ kadiwa-app/src/pagesConsumer/Products/GoodsCluster.jsx
@@ -9,26 +9,26 @@ function GoodsCluster() {
         <h1 className="text-xl text-black/80 font-bold mb-5 p-2 rounded-md tracking-widest">
           Categories
         </h1>
-        <div className="grid grid-cols-2 md:grid-cols-3 gap-3">
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-3">
           {foodCategory.map((foods, index) => (
             <NavLink
               to={`/main/products-page/${foods.category.toLowerCase()}`}
               key={index}
-              className="w-full p-1 border bg-gradient-to-br from-green-300 to-green-700 hover:bg-green-200 rounded-md shadow-md"
+              //className="w-full p-1 border bg-gradient-to-br from-green-300 to-green-700 hover:bg-green-200 rounded-md shadow-md"
               //className="w-full p-1 border bg-gradient-to-tr from-emerald-300 to-emerald-100 rounded-md shadow-md"
-              //className="w-full p-1 border bg-green-700 rounded-md shadow-md"
+              className="w-full p-1 border bg-slate-50 rounded-md shadow-md"
             >
               {foods.pic && (
                 <div className="flex justify-center">
                   <img
                     src={foods.pic}
                     alt={foods.category}
-                    className="w-full h-44 md:h-72 rounded-md object-cover"
+                    className="w-full h-32 md:h-56 rounded-md object-cover"
                     loading="lazy"
                   />
                 </div>
               )}
-              <h1 className="text-lg font-bold text-white text-center p-4 tracking-widest">
+              <h1 className="text-lg font-bold text-black/80 text-center p-1 tracking-widest">
                 {foods.category}
               </h1>
             </NavLink>
