895c3d0f70bb302646fb939172a1f12e1d3bdc85
K G A N <86733485+kganallinone@users.noreply.github.com>
2024-03-05 15:43:31 +0800

    a
    a

--- kadiwa-app/src/services/configurations/application.config.jsx
+++ kadiwa-app/src/services/configurations/application.config.jsx
@@ -1,14 +1,11 @@
-
 const BRAND = {
+  app_name: "Kadiwa App",
+  desc: "",
+  version: "beta-20240305.1",
+  created: {
+    date_start: "2024/01",
+    date_end: "2024/02/29",
+  },
+};
 
-    app_name: "Kadiwa App",
-    desc: "",
-    version: "beta-20240229.3",
-    created: {
-        date_start:"2024/01",
-        date_end:"2024/02/29",
-    }
-    
-}
-
-export {BRAND};
+export { BRAND };
