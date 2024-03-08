fb6bf1c9a72fa418b70376d55f054c69ccba7355
K G A N <86733485+kganallinone@users.noreply.github.com>
2024-03-08 17:19:30 +0800

    a
    a

--- kadiwa-app/src/App.jsx
+++ kadiwa-app/src/App.jsx
@@ -1,28 +1,27 @@
+import RouteLink from "./Routes/routeLink";
+import StoreLink from "./Routes/StoreLink";
+import POSLink from "./Routes/POSLink";
 
-
-import RouteLink  from './Routes/routeLink'
-import StoreLink from './Routes/StoreLink'
-import POSLink from './Routes/POSLink';
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import TestLink from './Routes/TestLink';
-import InfoRoute from './services/routes/information.route';
-import AuthRoute from './services/routes/auth.route';
-import MainRoute from './services/routes/main.route';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import TestLink from "./Routes/TestLink";
+import InfoRoute from "./services/routes/information.route";
+import AuthRoute from "./services/routes/auth.route";
+import MainRoute from "./services/routes/main.route";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/main/*" element={<MainRoute />} />
-        <Route path="/route/*" element={<RouteLink  />} />
-        <Route path="/partner/*" element={<StoreLink  />} />
-        <Route path="/pos/*" element={<POSLink  />} />
-        <Route path="/test/*" element={<TestLink  />} />
-        <Route path="/info/*" element={<InfoRoute  />} />
-        <Route path='/*' element={<AuthRoute/>}/>
+        <Route path="/route/*" element={<RouteLink />} />
+        <Route path="/partner/*" element={<StoreLink />} />
+        <Route path="/pos/*" element={<POSLink />} />
+        <Route path="/test/*" element={<TestLink />} />
+        <Route path="/info/*" element={<InfoRoute />} />
+        <Route path="/*" element={<AuthRoute />} />
       </Routes>
     </Router>
+    //ss
   );
 }
 
